df0c601c2de537a8cfa1c1d008fc8eaa43947fb2
Oleg Zhdanenko <Olegzhdan1988@gmail.com>
2024-02-26 11:09:17 +0300

    sum

--- src/components/friends/FriendListItem/FriendListItem.jsx
+++ src/components/friends/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,14 @@
 import clsx from "clsx";
 import css from "./FriendListitem.module.css"
 export const FriendListItem = ({ friendItem: { avatar, name, isOnline } }) => {
+    
     return (
         <div>
             <img src={avatar} alt="Avatar" width="48" />
             <p className={css.name}>{name}</p>
-            <p className={clsx(isOnline === "true" ? css.online : css.onlineOff)}>OnLine</p>
-            <p className={clsx(isOnline ==="false"? css.offline:css.offlineOff)}>OffLine</p>
+            <p className={clsx(css.status, isOnline === "true"? css.online : css.offline)}>
+				{isOnline ==="true" ? 'Online' : 'Offline'}
+			</p>
         </div>
     )
 };
